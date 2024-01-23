4b697ce1d89f09186d1046f1fb872d63cb38f04c
pascal-angelo <pascal_a3@aol.at>
2024-01-23 19:45:16 +0100

    added launcher icon, added run animation onCreate,

--- app/src/main/java/com/d11/space_cow_notes/MainActivity.kt
+++ app/src/main/java/com/d11/space_cow_notes/MainActivity.kt
@@ -2,6 +2,7 @@ package com.d11.space_cow_notes
 
 import android.content.Intent
 import android.database.CursorWindow
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
@@ -11,16 +12,35 @@ import java.util.TimerTask
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var scnAnimation: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.locations_main)
 
 
         // Logo Effect
-        val ivIcon = findViewById<ImageView>(R.id.ivIcon)
-        val zoomIn = AnimationUtils.loadAnimation(this, R.anim.rotate)
-        ivIcon.startAnimation(zoomIn)
+        //val ivIcon = findViewById<ImageView>(R.id.ivIcon)
+        //val zoomIn = AnimationUtils.loadAnimation(this, R.anim.rotate)
+        //ivIcon.startAnimation(zoomIn)
 
+        // Animation for SpaceCowNotes
+        val scnImage = findViewById<ImageView>(R.id.scn_image).apply {
+            setBackgroundResource(R.drawable.scn_anim)
+            scnAnimation = background as AnimationDrawable
+        }
+        // Run Animation for SpaceCowNotes with automatic start and loop
+        scnAnimation.apply {
+            isOneShot = false
+            start()
+        }
+        // When Animation is running, hide imageView src image
+        scnImage.apply {
+            if (scnAnimation.isRunning) {
+                setImageResource(0)
+            }
+        }
 
         // start OverviewActivity after 4 seconds
         val intent = Intent(this, OverviewActivity::class.java)
